b5f7eeb70e343762feaca85eff8faa319c74a1d0
Mahdi Hussain <mahdi.hussain.work@gmail.com>
2025-11-18 00:57:54 +0600

    solution 2 done

--- solution.ts
+++ solution.ts
@@ -13,3 +13,16 @@ const formatValue = (
 console.log(formatValue("hello"));
 console.log(formatValue(5));
 console.log(formatValue(true));
+
+const getLength = <T>(value: T): number => {
+    if (typeof value === "string") {
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    } else {
+        throw new Error("Invalid type");
+    }
+};
+
+console.log(getLength("typescript"));
+console.log(getLength([10, 20, 30, 40]));
